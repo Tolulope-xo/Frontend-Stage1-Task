7bdd9cf1218fcfacdb8c34b2ca13edaf5841f1e2
Tolulope-xo <olatunjitolulope4@gmail.com>
2022-11-02 09:25:18 -0700

    removed the targets in the a tag

--- src/Components/Buttons/buttons.js
+++ src/Components/Buttons/buttons.js
@@ -8,38 +8,36 @@ const buttons = () => {
     <div className="buttons1">
       <div className="buttons2">
         <button>
-          <a id="Ayo__tomiwa" target="_blank" href="#">
+          <a id="Ayo__tomiwa" href="#">
             Twitter Link
           </a>
         </button>
         <button>
-          <a id="btn__zuri" target="_blank" href="https://training.zuri.team">
+          <a id="btn__zuri" href="https://training.zuri.team">
             Zuri Team
           </a>
         </button>
         <button>
-          <a id="books" target="_blank" href="http://books.zuri.team">
+          <a id="books" href="http://books.zuri.team">
             Zuri Books
           </a>
         </button>
         <button>
           <a
             id="book__python"
-            target="_blank"
             href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>"
           >
             Python Books
           </a>
         </button>
         <button>
-          <a id="pitch" target="_blank" href="https://background.zuri.team">
+          <a id="pitch" href="https://background.zuri.team">
             Background Check for Coders
           </a>
         </button>
         <button>
           <a
             id="book__design"
-            target="_blank"
             href="https://books.zuri.team/design-rules"
           >
             Design Books
