7b4071c327a1dab09707e8790e188bea0dc964fe
Tolulope-xo <olatunjitolulope4@gmail.com>
2022-11-04 02:50:11 -0700

    style: made the footer responsive again

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import './contact.scss'
-import Form from '../../Components/Form/form'
-import Button from '../../Components/Button/button'
-import Footer from '../../Components/Footer/footer'
+import React from "react";
+import "./contact.scss";
+import Form from "../../Components/Form/form";
+import Button from "../../Components/Button/button";
+import Footer from "../../Components/Footer/footer";
 const contact = () => {
   return (
-    <div className='contact_page'>
+    <div className="contact_page">
+      <div className="contact">
         <h1>Contact Me</h1>
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
-        <Form/>
-        <Button/>
-        <Footer/>
-
+      </div>
+      <Form />
+      <Button />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default contact
+export default contact;
